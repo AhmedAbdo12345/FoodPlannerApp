02a54fc0b6f85a4c437079a64afd9ee5c10e79e4
youssef <youssefmohamed1999joemo@gmail.com>
2023-01-19 00:39:58 +0200

    connect forget password xml page with firebase

--- app/src/main/java/com/example/foodplanner/view/fragments/LoginFragment.java
+++ app/src/main/java/com/example/foodplanner/view/fragments/LoginFragment.java
@@ -1,12 +1,16 @@
 package com.example.foodplanner.view.fragments;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavHostController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,6 +18,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.foodplanner.R;
@@ -48,6 +53,7 @@ public class LoginFragment extends Fragment implements SignUpFragmentInterface, 
     LogInFragmentPresenter logInFragmentPresenter;
     EditText email, password;
     Button login;
+    TextView forgetTV;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -90,6 +96,14 @@ public class LoginFragment extends Fragment implements SignUpFragmentInterface, 
             public void onClick(View v) {
                 Intent intent = googleSignInClient.getSignInIntent();
                 startActivityForResult(intent, 100);
+            }
+        });
+        forgetTV=view.findViewById(R.id.forgetTextView);
+        forgetTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(LoginFragment.this).navigate(R.id.action_loginFragment_to_forgotPasswordFragment);
+
             }
         });
     }
